a90dbf1228ab496df9b4f89b0e9bf0b4e9132dcc
JS129 <JS129>
2021-09-16 00:17:45 +0530

    Add project files.

---
+++ EmergencyHelpLine/Data/EmergencyHelpLineContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmergencyHelpLine.Model;
+
+namespace EmergencyHelpLine.Data
+{
+    public class EmergencyHelpLineContext : DbContext
+    {
+        public EmergencyHelpLineContext (DbContextOptions<EmergencyHelpLineContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmergencyHelpLine.Model.Messages> Messages { get; set; }
+    }
+}
---
+++ EmergencyHelpLine/Migrations/20210915172542_Emergency.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmergencyHelpLine.Migrations
+{
+    public partial class Emergency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
